f8d63224cfd4b94c54b0195a642d238e4fb128e6
tdeheurles <tdeheurles@gmail.com>
2016-01-25 17:01:15 +0100

    Revert "move configuration to default.rb"
    This reverts commit 825b059323b5d13073120f661e73592e91558e16.

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -1,4 +1,3 @@
-# install
 include_recipe "apt"
 
 package 'nginx' do
@@ -10,43 +9,12 @@ service 'nginx' do
 end
 
 execute "chown" do
-  command "chown #{node.nginx.nginxUser} #{node.nginx.accessLog}"
-  command "chown #{node.nginx.nginxUser} #{node.nginx.errorLog}"
+  command "chown ubuntu /var/log/nginx"
+  command "chown ubuntu /var/log/nginx/error.log"
+  command "chown ubuntu /var/log/nginx/access.log"
 end
 
-# get files
-cookbook_file "/tmp/generate_certificates.sh" do
-  source "generate_certificates.sh"
-  mode "0755"
-end
-cookbook_file "/tmp/generate_nginx_conf.sh" do
-  source "generate_nginx_conf.sh"
-  mode "0755"
-end
-cookbook_file "/tmp/nginx.conf" do
+cookbook_file "/etc/nginx/nginx.conf" do
   source "nginx.conf"
   mode "0644"
-end
-
-# execute configuration
-execute "configure_nginx" do
-  command <<-EOF
-    cd /tmp
-    
-    ls -la
-    
-    ./generate_certificates.sh  \
-       #{node.nginx.domainName} \
-       #{node.nginx.certPath}
-    
-    ./generate_nginx_conf.sh      \
-       #{node.nginx.accessLog}    \
-       #{node.nginx.errorLog}     \
-       #{node.nginx.pemPath}      \
-       #{node.nginx.keyPath}      \
-       #{node.nginx.listenOn}     \
-       #{node.nginx.proxyPath}    \
-       #{node.nginx.nginxUser}    \
-       #{node.nginx.templatePath}
-    EOF
 end
