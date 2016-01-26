7ce3c6d3737064e89721f3e7caf9116492f1c401
tdeheurles <tdeheurles@gmail.com>
2016-01-26 11:03:01 +0100

    back to simple template

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -4,17 +4,29 @@ package 'nginx' do
   action :install
 end
 
-service 'nginx' do
-  action [ :enable, :start ]
-end
-
 execute "chown" do
   command "chown ubuntu /var/log/nginx"
   command "chown ubuntu /var/log/nginx/error.log"
+  command "echo "" > /var/log/nginx/error.log"
   command "chown ubuntu /var/log/nginx/access.log"
+  command "echo "" > /var/log/nginx/access.log"
 end
 
 cookbook_file "/etc/nginx/nginx.conf" do
   source "nginx.conf"
-  mode "0644"
+  owner  "ubuntu"
+  mode   "0644"
+end
+cookbook_file "/tmp/generate_certs.sh" do
+  source "generate_certs.sh"
+  owner  "ubuntu"
+  mode   "0755"
+end
+
+execute "certs" do
+  command "cd /tmp && ./generate_certs.sh nginxtest"
+end
+
+service 'nginx' do
+  action [ :enable, :stop, :start ]
 end
